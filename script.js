1a05873de7ea29faabbc92c73288ad310b742011
yungbow <buyungarif.budpar@gmail.com>
2020-04-20 11:21:23 +0700

    Lesson 10. Apa yang dimaksud dengan Object?

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const animals = ["anjing", "kucing", "domba", "kelinci", "monyet", "harimau", "beruang", "gajah"];
+// Deklarasikan constant character dan berikan object yang sudah disediakan
+const character = {name: "Ninja Ken", age: 14};
 
-// Gunakan property length untuk mem-print jumlah element di array
-console.log(animals.length);
-
-// Gunakan property length untuk mengubah kondisi dibawah
-for (let i = 0; i < animals.length; i++) {
-  console.log(animals[i]);
-}
+// Print nilai character
+console.log(character);
