080158ea062c41c490e8d9041b5f289c441bac47
yungbow <buyungarif.budpar@gmail.com>
2020-04-20 11:29:44 +0700

    Lesson 12. Array dalam Object (1)

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const character = {name: "Ninja Ken", age: 14};
+const characters = [
+  {name: "Ninja Ken", age: 14},
+  {name: "Guru Domba", age: 1000}
+];
 
-// Print nilai property name milik character
-console.log(character.name);
+// Print element pertama milik array characters
+console.log(characters[0]);
 
-// Gantikan nilai age milik character ke 20
-character.age = 20;
-
-// Print constant character ke console
-console.log(character);
+// Print nilai milik property name dari element array character kedua
+console.log(characters[1].name);
